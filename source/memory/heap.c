a92768c3aaafb8def3ef33d84a524d229e11ece2
ostef <47391841+ostef@users.noreply.github.com>
2022-07-29 21:33:00 +0200

    Added a define to disable counting heap allocations.

--- source/memory/heap.c
+++ source/memory/heap.c
@@ -6,13 +6,17 @@
 /*   By: soumanso <soumanso@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 17:47:39 by soumanso          #+#    #+#             */
-/*   Updated: 2022/02/25 14:20:04 by soumanso         ###   ########lyon.fr   */
+/*   Updated: 2022/07/29 21:30:34 by soumanso         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#ifdef COUNT_HEAP_ALLOCATIONS
+
 static t_int	g_heap_allocations;
+
+#endif
 
 t_alloc	ft_heap(void)
 {
@@ -23,10 +27,23 @@ t_alloc	ft_heap(void)
 	return (result);
 }
 
+#ifdef COUNT_HEAP_ALLOCATIONS
+
 t_int	ft_get_heap_allocations(void)
 {
 	return (g_heap_allocations);
 }
+
+#else
+
+t_int	ft_get_heap_allocations(void)
+{
+	return (0);
+}
+
+#endif
+
+#ifdef COUNT_HEAP_ALLOCATIONS
 
 void	*ft_heap_alloc(t_alloc_op op, t_s64 size, void *ptr, void *data)
 {
@@ -48,3 +65,24 @@ void	*ft_heap_alloc(t_alloc_op op, t_s64 size, void *ptr, void *data)
 	}
 	return (NULL);
 }
+
+#else
+
+void	*ft_heap_alloc(t_alloc_op op, t_s64 size, void *ptr, void *data)
+{
+	void	*result;
+
+	(void)data;
+	if (op == OP_ALLOCATE)
+	{
+		result = malloc (size);
+		return (result);
+	}
+	else if (op == OP_FREE)
+	{
+		free (ptr);
+	}
+	return (NULL);
+}
+
+#endif
