71519d141265023857aec8f8575352a76022038f
ostef <47391841+ostef@users.noreply.github.com>
2022-08-25 22:45:06 +0200

    Use DebugBreak function in ft_debug_break, because inline assembly is compiler specific.

--- source/debug.c
+++ source/debug.c
@@ -6,16 +6,27 @@
 /*   By: soumanso <soumanso@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 16:28:00 by soumanso          #+#    #+#             */
-/*   Updated: 2021/11/22 17:11:49 by soumanso         ###   ########lyon.fr   */
+/*   Updated: 2022/08/25 22:44:27 by soumanso         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#ifdef FT_WIN32
+
+void	ft_debug_break(void)
+{
+	DebugBreak ();
+}
+
+#else
+
 void	ft_debug_break(void)
 {
 	asm ("int3");
 }
+
+#endif
 
 void	ft_assert(t_bool assert, t_cstr fmt, ...)
 {
